20986d620242d9fb231338b57ba934322cd9bd44
Sophie Au <SophieAu@users.noreply.github.com>
2020-08-30 18:03:23 +0200

    Add footer content to schema

--- studio/schemas/documents/siteSettings.js
+++ studio/schemas/documents/siteSettings.js
@@ -13,6 +13,16 @@ export default {
       name: "title",
       type: "string",
       title: "Title"
+    },
+    {
+      name: "copyrightHolder",
+      type: "string",
+      title: "Official Club Name"
+    },
+    {
+      name: "footerText",
+      type: "string",
+      title: "Footer Text"
     }
   ]
 };

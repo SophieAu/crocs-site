30787fab6a55fc5366f0332827d71606237ea331
Sophie Au <SophieAu@users.noreply.github.com>
2020-09-02 20:31:22 +0200

    Fix wrong field name

--- studio/schemas/objects/home/practice.js
+++ studio/schemas/objects/home/practice.js
@@ -5,7 +5,7 @@ export default {
   options: { collapsible: true },
   fields: [
     { title: "Title", name: "title", type: "string" },
-    { title: "Open Street Map URL", name: "subtitle", type: "url" },
+    { title: "Open Street Map URL", name: "osm_url", type: "url" },
     { title: "Text", name: "text", type: "body" }
   ]
 };

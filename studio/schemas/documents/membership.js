bf1880b722192376d50662b7815e62f296d9e766
Sophie Au <SophieAu@users.noreply.github.com>
2020-09-06 17:00:35 +0200

    Remove excerpt and add button text

--- studio/schemas/documents/membership.js
+++ studio/schemas/documents/membership.js
@@ -8,6 +8,6 @@ export default {
     { name: "name", type: "string", title: "Card Title" },
     { name: "image", type: "image", title: "Card Image" },
     { name: "description", type: "body", title: "Description" },
-    { name: "excerpt", type: "text", title: "Excerpt" }
+    { name: "button_text", type: "string", title: "Button Text" }
   ]
 };

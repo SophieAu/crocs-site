ff177af7404781e7158b4a8640358c8cdcca1dc1
Sophie Au <SophieAu@users.noreply.github.com>
2020-11-15 11:48:55 +0100

    Style studio

---
+++ studio/logo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './logo.css';
+
+const Logo = () => (
+  <img src="/static/logo.png" alt="Berlin Crocodiles" className={styles.logo}/>
+);
+
+export default Logo;
